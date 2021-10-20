e95cc4b7b19f4d1589ca25863b826385bbc3409c
Daniel Rashba <danirdd92@gmail.com>
2021-10-20 18:56:28 +0300

    removed esbuild-wasm from public dir, fetching dependency moved to unpkg

--- src/index.tsx
+++ src/index.tsx
@@ -12,7 +12,7 @@ const App = () => {
 		try {
 			await esbuild.initialize({
 				worker: true,
-				wasmURL: '/esbuild.wasm',
+				wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm',
 			});
 		} catch (err) {
 			console.log(err);
