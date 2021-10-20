e1567ce53ed964084ab2c3fdaf5355354e4d060d
Daniel Rashba <danirdd92@gmail.com>
2021-10-20 18:50:57 +0300

    Added ESBuild with middleware
    Overriden fs access in order to fetch packages to get required
    dependencies from unpkg CDN

---
+++ src/index.tsx
@@ -0,0 +1,58 @@
+import * as esbuild from 'esbuild-wasm';
+import { useEffect, useState } from 'react';
+import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
+import { fetchPlugin } from './plugins/fetch-plugin';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+	const [input, setInput] = useState('');
+	const [code, setCode] = useState('');
+
+	const startService = async () => {
+		try {
+			await esbuild.initialize({
+				worker: true,
+				wasmURL: '/esbuild.wasm',
+			});
+		} catch (err) {
+			console.log(err);
+		}
+	};
+
+	useEffect(() => {
+		startService();
+	}, []);
+
+	const onClick = async () => {
+		try {
+			const res = await esbuild.build({
+				entryPoints: ['index.js'],
+				bundle: true,
+				write: false,
+				plugins: [unpkgPathPlugin(), fetchPlugin(input)],
+				define: {
+					'process.env.NODE_ENV': '"production"',
+					global: 'window',
+				},
+			});
+			setCode(res.outputFiles[0].text);
+		} catch (err) {
+			console.error(err);
+		}
+	};
+
+	return (
+		<div>
+			<textarea value={input} onChange={(e) => setInput(e.target.value)}>
+				{input}
+			</textarea>
+
+			<div>
+				<button onClick={onClick}>Submit</button>
+			</div>
+			<pre>{code}</pre>
+		</div>
+	);
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
